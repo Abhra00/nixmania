f152632ac5a369b15d2ecb913901bfd4d71109f7
Abhra00 <114018452+Abhra00@users.noreply.github.com>
2026-06-26 15:40:48 +0530

    Revamp tmux theme

--- modules/desktop/theming/stylix/overrides/tmux.nix
+++ modules/desktop/theming/stylix/overrides/tmux.nix
@@ -9,29 +9,15 @@
   in {
     hm.programs.tmux.extraConfig = lib.mkIf cfg ''
       # Theme
-      set -g pane-border-lines simple
       set -g status-style "bg=default,fg=default"
-
-      # Left: session name pill
-      set -g status-left "#{?client_prefix,#[fg=#${c.base0D}],#[fg=#${c.base08}]}#[bg=default]#{?client_prefix,#[bg=#${c.base0D}],#[bg=#${c.base08}]}#[fg=#${c.base00}] #[bg=#${c.base02},fg=#${c.base05}] #S#[fg=#${c.base02},bg=default] "
-      set -g status-left-length 40
-
-      # Right: date pill
-      set -g status-right "#[fg=#${c.base0A},bg=default]#[fg=#${c.base00},bg=#${c.base0A}]󰃰 #[bg=#${c.base02},fg=#${c.base05}] %Y-%m-%d %H:%M#[fg=#${c.base02},bg=default]"
-      set -g status-right-length 40
-
-      # Windows
-      set -g window-status-format "#[fg=#${c.base0D},bg=default]#[bg=#${c.base0D},fg=#${c.base00}]#I #[bg=#${c.base02},fg=#${c.base03}] #W#[fg=#${c.base02},bg=default] "
-      set -g window-status-current-format "#[fg=#${c.base0A},bg=default]#[bg=#${c.base0A},fg=#${c.base00}]#I #[bg=#${c.base02},fg=#${c.base05}] #W#[fg=#${c.base02},bg=default] "
-      set -g window-status-separator ""
-
-      # Pane borders
-      set -g pane-border-style "fg=#${c.base02}"
+      set -g status-left "#[fg=#${c.base00},bg=#${c.base0D},bold] #S #[bg=default] "
+      set -g status-right "#[fg=#${c.base0D}]#{?pane_in_mode,COPY ,}#{?client_prefix,PREFIX ,}#{?window_zoomed_flag,ZOOM ,}#[fg=#${c.base03}]#h "
+      set -g window-status-format "#[fg=#${c.base03}] #I:#W "
+      set -g window-status-current-format "#[fg=#${c.base0D},bold] #I:#W "
+      set -g pane-border-style "fg=#${c.base03}"
       set -g pane-active-border-style "fg=#${c.base0D}"
-
-      # Messages / copy mode
-      set -g message-style "bg=#${c.base02},fg=#${c.base0D}"
-      set -g message-command-style "bg=#${c.base02},fg=#${c.base0D}"
+      set -g message-style "bg=default,fg=#${c.base0D}"
+      set -g message-command-style "bg=default,fg=#${c.base0D}"
       set -g mode-style "bg=#${c.base0D},fg=#${c.base00}"
       setw -g clock-mode-colour "#${c.base0D}"
     '';

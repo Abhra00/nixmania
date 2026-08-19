e99836f6c6b0eabb4597b7de1c6dff7c3f5bc764
Abhra00 <114018452+Abhra00@users.noreply.github.com>
2026-08-19 21:10:22 +0530

    nixmania: NixOS configuration

---
+++ modules/desktop/theming/stylix/fonts.nix
@@ -0,0 +1,78 @@
+{
+  flake.modules.nixos.theming_fonts = {
+    pkgs,
+    inputs,
+    ...
+  }: let
+    inkwell = {
+      sansSerif = {
+        name = "Inter";
+        package = pkgs.inter;
+      };
+      serif = {
+        name = "Lora";
+        package = pkgs.lora;
+      };
+      monospace = {
+        name = "JetBrains Mono";
+        package = pkgs.jetbrains-mono;
+      };
+    };
+  in {
+    stylix.fonts =
+      inkwell
+      // {
+        emoji = {
+          name = "Noto Color Emoji";
+          package = pkgs.noto-fonts-color-emoji;
+        };
+        sizes = {
+          applications = 10;
+          desktop = 10;
+          popups = 10;
+          terminal = 12;
+        };
+      };
+    hm.fonts.fontconfig.configFile.monospace-fallback = {
+      enable = true;
+      priority = 55;
+      text = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+        <fontconfig>
+          <alias>
+            <family>monospace</family>
+            <prefer>
+              <family>${inkwell.monospace.name}</family>
+              <family>Symbols Nerd Font Mono</family>
+            </prefer>
+          </alias>
+        </fontconfig>
+      '';
+    };
+    hm.fonts.fontconfig.configFile.bengali-fallback = {
+      enable = true;
+      priority = 60;
+      text = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+        <fontconfig>
+        <alias>
+        <family>sans-serif</family>
+        <prefer>
+          <family>${inkwell.sansSerif.name}</family>
+          <family>Noto Sans Bengali</family>
+        </prefer>
+        </alias>
+        <alias>
+        <family>serif</family>
+        <prefer>
+          <family>${inkwell.serif.name}</family>
+          <family>Noto Serif Bengali</family>
+        </prefer>
+        </alias>
+        </fontconfig>
+      '';
+    };
+  };
+}
